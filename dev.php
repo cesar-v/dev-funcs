960f5eff6977617a4b3692bcf2f9de6f45c3b0bb
Cesar Vega <Cesar.Vega@wnc.edu>
2014-12-05 15:54:21 -0800

    Added speed() benchmarking function.

--- dev.php
+++ dev.php
@@ -34,7 +34,7 @@ if ( ! function_exists('dd'))
     }
 }
 
-if ( ! function_exist('d'))
+if ( ! function_exists('d'))
 {
     /**
      * Short cut to var_dump();
@@ -44,5 +44,42 @@ if ( ! function_exist('d'))
     function d()
     {
         var_dump(\func_get_args());
+    }
+}
+
+if ( ! function_exists('speed'))
+{
+    /**
+     * Check speed of given call back.
+     * 
+     * @param closure $callback
+     * @param int $iterations
+     * @param boolean $displayDetails
+     * @return int
+     */
+    function speed($callback, $iterations = 25, $displayDetails = false)
+    {
+        $times = array();
+
+        for($i = 0; $i < $iterations; $i++) {
+
+            $start = microtime(true);
+            call_user_func($callback);
+            $elapsedTime = microtime(true) - $start;
+
+            $times[] = $elapsedTime;
+
+            if ($displayDetails) {
+                echo 'Iteration ' . $i . ': ' . $elapsedTime . '<hr/>';
+            }
+        }
+
+        $average = array_sum($times) / $iterations;
+
+        if ($displayDetails) {
+            echo 'Average: ' . $average;
+        } 
+
+        return $average;
     }
 }
